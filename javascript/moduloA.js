bbc77eb9b062eb88af353a2585f71247b44a7590
Pedro Santos <pedrovisk.dev@gmail.com>
2024-07-17 20:28:28 -0400

    Aula 17/08

---
+++ javascript/moduloA.js
@@ -0,0 +1,7 @@
+const array = require("./moduloB")
+
+
+
+array.forEach(objeto => {
+    console.log(objeto["time"]);
+});g
---
+++ javascript/moduloB.js
@@ -0,0 +1,14 @@
+const array = [
+    {
+        time: "Vasco",
+        titulo: "Campeão da série B"
+    },
+    {
+        time: "Flamengo",
+        titulo: "Campeão Copa do Brasil"
+    }
+]
+
+
+module.exports = array
+
